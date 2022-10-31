1f673d21eec19a7c38b64356ad8bc1b2615252d6
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-10-31 11:39:07 -0300

    Create calculadoradora.py

---
+++ .github/workflows/calculadoradora.py
@@ -0,0 +1,39 @@
+''def menu():
+    print("1-soma")
+    print("2-subtração")
+    print("3-multiplicação")
+    print("4-divisão")
+    print("5-sair do sistema")
+
+
+
+def somar():
+    if num==1:
+        num1=float(input("Digite um numero para a soma"))
+        num2=float(input("Digite um numero pra a soma"))
+        cal=num1+num2
+        print("A soma dos dois numeros é: ",cal)
+
+def subtracao():
+    if num==2:
+        num3=float(input("Digite um numero para  subtrair"))
+        num4=float(input("Digite um numero pra  subtrair"))
+        cal=num3-num4
+        print("A subtração dos dois numeros é: ",cal)
+
+
+def multiplicacao():
+    if num==3:
+        num3=float(input("Digite um numero para  multiplicar"))
+        num4=float(input("Digite um numero pra  multiplicar"))
+        cal=num3*num4
+        print("A multiplicação dos dois numeros é: ",cal)
+
+def divisao():
+    
+        if num==4:
+
+            num3=float(input("Digite um numero para dividir "))
+            num4=float(input("Digite um numero pra dividir "))
+            cal=num3/num4
+            print("A divisão dos dois numeros é: ",cal)
