118666322cd74710c3637c74098a04274345c273
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-11-07 11:06:57 -0300

    Create mainForma.py

---
+++ .github/fluxos de trabalho/mainForma.py
@@ -0,0 +1,10 @@
+#caculo da droma
+f1=Forma(10,10)
+print(f"o valor da base do perimetro é {f1.perimetro} e o valor da base da  area é {f1.area}")
+#calculando a area do triangulo
+f2=Triangulo(10,10)
+f2.calculaArea(2,2)
+#calculando a area do retangulo
+f3=Retangulo(10,10)
+f3.calcularPerimetro(4,4)
+
