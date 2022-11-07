6936dfa2485b1b31b340150bcd642a58ba5daa6c
Orlando Paulo <113864263+Orlando-Paulo99@users.noreply.github.com>
2022-11-07 11:25:48 -0300

    Update and rename biblioteca.py to bibliotecaAtleta.py

--- .github/workflows/bibliotecaAtleta.py
+++ .github/workflows/bibliotecaAtleta.py
@@ -1,19 +1,3 @@
-class Forma():
-    def __init__(self,area,perimetro):
-        self.area=area
-        self.perimetro=perimetro
-
-class Triangulo(Forma):
-    def calculaArea(self,base,altura):
-
-        self.area=base+altura**2
-        print(f"o valor da area  dp triangulo é: {self.area}")
-
-class Retangulo(Forma):
-    def calcularPerimetro(self,base,altura):
-        self.perimetro=(base+altura)**2
-        print(f"o valor da area do perimetro é: {self.perimetro}")
-
 
 class Atleta():
     def __init__(self,peso,aposentado=False):
